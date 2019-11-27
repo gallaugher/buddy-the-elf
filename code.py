e8e609975a24a1a13547ca5a0191777cab4ae58e
Gallaugher <john.gallaugher@bc.edu>
2019-11-26 22:24:57 -0500

    Updated for CircuitPython Beta 0
    Updated Bluetooth & more files / better wiring match.

--- code.py
+++ code.py
@@ -12,13 +12,17 @@ import pulseio
 from adafruit_motor import servo
 import random
 
-from adafruit_ble.uart_server import UARTServer
-
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.color_packet import ColorPacket
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
 
-uart_server = UARTServer()
+# setup bluetooth
+ble = BLERadio()
+uart_server = UARTService()
+advertisement = ProvideServicesAdvertisement(uart_server)
 
 # enable the speaker
 speaker = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
@@ -54,7 +58,6 @@ button_A.switch_to_input(pull=digitalio.Pull.DOWN)
 button_B = digitalio.DigitalInOut(board.BUTTON_B)
 button_B.switch_to_input(pull=digitalio.Pull.DOWN)
 
-
 # set up touchpads
 # removed board.TX for lid_servo
 pads = [board.A2, board.A3, board.A4, board.A5, board.A6]
@@ -81,7 +84,11 @@ lid_servo = servo.Servo(pwm_lid, min_pulse  = 500, max_pulse = 2250)
 lid_servo.angle = 115
 
 # define the drumsamples list
-buddySounds = ["my-names-buddy.wav", "christmas-cheer-sing-loud.wav",  "its-great-to-meet-you.wav", "i-just-like-to-smile.wav", "santas-coming-so-much-to-do.wav", "you-sit-on-a-throne-of-lies.wav"]
+buddySounds = ["my-names-buddy.wav", "christmas-cheer-sing-loud.wav", 
+    "santas-coming-so-much-to-do.wav", "its-great-to-meet-you.wav", 
+    "i-just-like-to-smile.wav", "you-sit-on-a-throne-of-lies.wav", 
+    "you-did-it.wav", "whats-a-christmas-gram.wav", "i-love-you.wav", "santa-i-know-him.wav", 
+    "santa-must-have-called-you.wav"]
 
 startAngle = 0
 endAngle = 135
@@ -98,43 +105,50 @@ def playfile(filename):
             moveBack()
 
 def moveUp():
-    print("*** MOVE UP! ***")
+    # print("*** MOVE UP! ***")
     for angle in range(startAngle, 50, 3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        print(angle)
+        # print(angle)
         time.sleep(0.02)
 
 def moveBack():
-    print("*** MOVE BACK! ***")
+    # print("*** MOVE BACK! ***")
     for angle in range(49, startAngle, -3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        print(angle)
+        # print(angle)
         time.sleep(0.02)
 
 def move():
     # for i in range(2):
     for angle in range(50, endAngle, 3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        print(angle)
+        # print(angle)
         time.sleep(0.02)
     for angle in range(endAngle, 49, -3): # 180 - 0 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        print(angle)
+        # print(angle)
         time.sleep(0.02)
-    print("*** DONE ***")
+    # print("*** DONE ***")
     # wave_servo.angle = startAngle
     print(startAngle)
 
 def checkTouch():
     for i in range(len(touchpad)):
         if touchpad[i].value:
-            if i == 2:
+            print("Touchpad", i ,"was touched!")
+            if i == 4:
                 openLid()
+                print("touchpad ", i)
             elif i == 3:
-                soundToPlay = random.randint(0, len(buddySounds))
+                soundToPlay = random.randint(3, len(buddySounds) - 1)
+                print("touchpad ", i, "sound", soundToPlay)
                 playfile(buddySounds[soundToPlay])
-            else:
+            else: # should be 0, 1, or 2
+                print("touchpad ", i, "sound", i)
                 playfile(buddySounds[i])
+                # if i == 4:
+                    # playfile(buddySounds[2])
+                
 
 def openLid():
     for i in range(116):
@@ -145,38 +159,45 @@ def openLid():
 
 
 while True:
-    uart_server.start_advertising()
-    while not uart_server.connected:
+    # set CPXb up so that it can be discovered by the app
+    ble.start_advertising(advertisement)
+    while not ble.connected:
         checkTouch()
-        pass
+    ble.stop_advertising()
 
     # Now we're connected
 
-    while uart_server.connected:
+    while ble.connected:
         checkTouch()
-        if uart_server.in_waiting:
-            try:
-                packet = Packet.from_stream(uart_server)
-            except ValueError:
-                continue # or pass. This will start the next iteration of the loop and try to get a valid packet again.
+        try:
+            packet = Packet.from_stream(uart_server)
+        except ValueError:
+            continue # or pass. This will start the next iteration of the loop and try to get a valid packet again.
 
-            if isinstance(packet, ButtonPacket):
-                if packet.pressed:
-                    if packet.button == ButtonPacket.BUTTON_1:
-                        playfile(buddySounds[0])
-                    if packet.button == ButtonPacket.BUTTON_2:
-                        print("*** 1")
-                        playfile(buddySounds[1])
-                    if packet.button == ButtonPacket.BUTTON_3:
-                        print("*** 2")
-                        playfile(buddySounds[2])
-                    if packet.button == ButtonPacket.BUTTON_4:
-                        print("*** 3")
-                        soundToPlay = random.randint(0, len(buddySounds) - 1)
-                        playfile(buddySounds[soundToPlay])
-                    if packet.button == ButtonPacket.UP:
-                        openLid()
-                    if packet.button == ButtonPacket.DOWN:
-                        playfile(buddySounds[5])
-                    if packet.button == ButtonPacket.LEFT:
-                        playfile(buddySounds[4])
+        if isinstance(packet, ButtonPacket):
+            if packet.pressed:
+                if packet.button == ButtonPacket.BUTTON_1:
+                    playfile(buddySounds[0])
+                if packet.button == ButtonPacket.BUTTON_2:
+                    print("*** 1")
+                    playfile(buddySounds[1])
+                if packet.button == ButtonPacket.BUTTON_3:
+                    print("*** 2")
+                    playfile(buddySounds[2])
+                if packet.button == ButtonPacket.BUTTON_4:
+                    print("*** 3")
+                    soundToPlay = random.randint(0, len(buddySounds) - 1)
+                    playfile(buddySounds[soundToPlay])
+                if packet.button == ButtonPacket.UP:
+                    openLid()
+                if packet.button == ButtonPacket.DOWN:
+                    playfile(buddySounds[5])
+                if packet.button == ButtonPacket.LEFT:
+                    playfile(buddySounds[4])
+                if packet.button == ButtonPacket.RIGHT:
+                    for i in range(len(touchpad)):
+                        print("Before Change: pad", i, "threshold = ", touchpad[i].threshold)
+                        touchpad[i].threshold = touchpad[i].threshold + 50
+                    for i in range(len(touchpad)):
+                        print("After Change: pad", i, "threshold = ", touchpad[i].threshold)
+                        touchpad[i].threshold = touchpad[i].threshold + 50
