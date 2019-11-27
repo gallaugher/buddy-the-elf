c117adb235a59bf97d4de29290decc88488a9c40
Gallaugher <john.gallaugher@bc.edu>
2019-11-27 17:14:52 -0500

    Removed extra code + print statements

--- code.py
+++ code.py
@@ -1,5 +1,4 @@
 import board
-import neopixel
 import time
 import digitalio
 import touchio
@@ -29,35 +28,6 @@ speaker = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
 speaker.direction = digitalio.Direction.OUTPUT
 speaker.value = True
 
-# sets up 10 lights on the CPXb board and names them pixels
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.3, auto_write=True)
-
-# name colors so you don't need to refer to numbers
-RED = (255, 0, 0)
-ORANGE = (255, 50, 0)
-YELLOW = (255, 165, 0)
-LIGHT_GREEN = (165, 255, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-INDIGO = (50, 0, 255)
-VIOLET = (75, 0, 130)
-PINK = (255, 0, 255)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-# an array of colors
-colors = [RED, ORANGE, YELLOW, LIGHT_GREEN, GREEN, CYAN, BLUE, INDIGO, VIOLET, PINK]
-
-pixels.fill(BLACK)
-
-# define buttons A + B
-button_A = digitalio.DigitalInOut(board.BUTTON_A)
-button_A.switch_to_input(pull=digitalio.Pull.DOWN)
-
-button_B = digitalio.DigitalInOut(board.BUTTON_B)
-button_B.switch_to_input(pull=digitalio.Pull.DOWN)
-
 # set up touchpads
 # removed board.TX for lid_servo
 pads = [board.A2, board.A3, board.A4, board.A5, board.A6]
@@ -68,8 +38,6 @@ touchpad = []
 # loop through all elements of pads and create the touchpad list
 for i in range(len(pads)):
     touchpad.append(touchio.TouchIn(pads[i]))
-
-armed = False
 
 # create a PWMOut object on Pin A1.
 pwm = pulseio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
@@ -84,10 +52,10 @@ lid_servo = servo.Servo(pwm_lid, min_pulse  = 500, max_pulse = 2250)
 lid_servo.angle = 115
 
 # define the drumsamples list
-buddySounds = ["my-names-buddy.wav", "christmas-cheer-sing-loud.wav", 
-    "santas-coming-so-much-to-do.wav", "its-great-to-meet-you.wav", 
-    "i-just-like-to-smile.wav", "you-sit-on-a-throne-of-lies.wav", 
-    "you-did-it.wav", "whats-a-christmas-gram.wav", "i-love-you.wav", "santa-i-know-him.wav", 
+buddySounds = ["my-names-buddy.wav", "christmas-cheer-sing-loud.wav",
+    "santas-coming-so-much-to-do.wav", "its-great-to-meet-you.wav",
+    "i-just-like-to-smile.wav", "you-sit-on-a-throne-of-lies.wav",
+    "you-did-it.wav", "whats-a-christmas-gram.wav", "i-love-you.wav", "santa-i-know-him.wav",
     "santa-must-have-called-you.wav"]
 
 startAngle = 0
@@ -105,32 +73,22 @@ def playfile(filename):
             moveBack()
 
 def moveUp():
-    # print("*** MOVE UP! ***")
     for angle in range(startAngle, 50, 3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        # print(angle)
         time.sleep(0.02)
 
 def moveBack():
-    # print("*** MOVE BACK! ***")
     for angle in range(49, startAngle, -3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        # print(angle)
         time.sleep(0.02)
 
 def move():
-    # for i in range(2):
     for angle in range(50, endAngle, 3):  # 0 - 180 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        # print(angle)
         time.sleep(0.02)
     for angle in range(endAngle, 49, -3): # 180 - 0 degrees, 5 degrees at a time.
         wave_servo.angle = angle
-        # print(angle)
         time.sleep(0.02)
-    # print("*** DONE ***")
-    # wave_servo.angle = startAngle
-    print(startAngle)
 
 def checkTouch():
     for i in range(len(touchpad)):
@@ -146,9 +104,6 @@ def checkTouch():
             else: # should be 0, 1, or 2
                 print("touchpad ", i, "sound", i)
                 playfile(buddySounds[i])
-                # if i == 4:
-                    # playfile(buddySounds[2])
-                
 
 def openLid():
     for i in range(116):
@@ -156,7 +111,6 @@ def openLid():
         print(i)
         if i > 10:
             time.sleep(0.05)
-
 
 while True:
     # set CPXb up so that it can be discovered by the app
